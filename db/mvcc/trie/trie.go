a2967ab23e38f17e99183ba480b327395dd960a0
ziranliu <ziran@iost.io>
2018-09-19 04:52:40 +0800

    bug fix
    Change-Id: Icf7a3b87c0f8ccf2d02dbc4f08e0952b9dda3e7e

--- db/mvcc/trie/trie.go
+++ db/mvcc/trie/trie.go
@@ -160,7 +160,7 @@ func (c *Context) newNode() *Node {
 func (c *Context) freeNode(n *Node) {
 	n.context = nil
 	n.value = nil
-	n.children = nil
+	n.children = make(map[byte]*Node)
 	c.freelist.freeNode(n)
 	pool.Put(n)
 }
